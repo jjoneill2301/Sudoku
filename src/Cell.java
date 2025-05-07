24da393c54c74caa8ac7ef687ac75f35cefa4392
johnj <jjoneill2301@student.stcc.edu>
2025-05-07 16:08:22 -0400

    Reformatted, better comments

--- src/Cell.java
+++ src/Cell.java
@@ -1,8 +1,7 @@
 public class Cell {
-    private int value; // Can be 0 for empty, or 1-9 for a number
-    private final int row;  // In Java, a protected variable is a class member that can be accessed within the same class,
-    private final int col; // by subclasses, and by other classes within the same package. -Google AI Summarization
-    // Can be final because once the application gets to the user, they user is now only changing the JTable
+    private final int row;  // Row and col can both be final because
+    private final int col;  // Cells will never change coordinates
+    private int value;      // Can be 0 for empty, or 1-9 for a number
 
     public Cell(int row, int col) {
         this.row = row;
@@ -10,7 +9,7 @@ public class Cell {
         this.value = 0;
     }
 
-    public int getValue() {
+    public int getValue() { // Originally had mutators for col/row, but SudokuBoardWithCells already has access
         return value;
     }
 
@@ -18,17 +17,14 @@ public class Cell {
         this.value = value;
     }
 
-    //  I initially made getters and setters for col/row, but because I am looping through the rows/columns within my
-    //  SudokuBoardWithCells.java class, I already have access to that information within scope while I would want it.
-
     public boolean isValid(Cell[][] grid, int num) {
         for (int c = 0; c < 9; c++) {
-            if(grid[row][c].value == num) { // if value of cell == num
+            if (grid[row][c].value == num) { // if value of cell == num
                 return false;
             } // Num not valid if found at any point on y level row
         }
         for (int r = 0; r < 9; r++) { // if value of cell == num
-            if(grid[r][col].value == num) {
+            if (grid[r][col].value == num) {
                 return false;
             } // Num not valid if found at any point on x level col
         }
